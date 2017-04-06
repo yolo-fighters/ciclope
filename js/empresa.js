ff8abf3874125a8fd4f0fd204403d4b72938a00f
sayda <sayda>
2017-04-05 21:50:10 -0500

    html perdiste y ganste

--- js/empresa.js
+++ js/empresa.js
@@ -5,7 +5,7 @@ function init(){
     $('#inicio').click(secInicio);
 }
 
-
+//FUNCION PARA LISTA DE COSAS GRATIS
 function htmlImagenes (){
     console.log('loca..!');
     var listaImg=[
@@ -23,20 +23,18 @@ function htmlImagenes (){
     }
     $('#lista').html(carga);
 }
-
+//FUNCIONES PARA NAVEGAR SECIONES
 function escanear(){
     $('#jugadores').hide();
     $('#sectionScanea').show();
 }
 
-
-
 function secInicio(){
     $('#jugadores').show();
     $('#sectionScanea').hide();
 }
-
-
-$(function (){
-$("#datepicker").datepicker();
+//FUNCION PARA EL CALENDARIO
+$(function(){
+    $("#datepicker").datepicker();
+    $("#datepicker2").datepicker();
 });
